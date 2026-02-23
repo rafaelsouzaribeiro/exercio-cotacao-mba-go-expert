fa9bf5b03fcec4ed92679d34589d61608c48ebd3
Rafael <rafaelribeirosouza86@gmail.com>
2026-02-23 16:03:17 -0300

    first commit

--- internal/infra/web/handlers/cambio.go
+++ internal/infra/web/handlers/cambio.go
@@ -1,1 +1,23 @@
 package handlers
+
+import "net/http"
+
+type Cambio struct {
+	USDBRL struct {
+		Code       string `json:"code"`
+		Codein     string `json:"codein"`
+		Name       string `json:"name"`
+		High       string `json:"high"`
+		Low        string `json:"low"`
+		VarBid     string `json:"varBid"`
+		PctChange  string `json:"pctChange"`
+		Bid        string `json:"bid"`
+		Ask        string `json:"ask"`
+		Timestamp  string `json:"timestamp"`
+		CreateDate string `json:"create_date"`
+	} `json:"USDBRL"`
+}
+
+func (h *Usecase) Cambio(w http.ResponseWriter, r *http.Request) {
+
+}
